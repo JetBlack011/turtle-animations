42cdea4db5b8d3a8b0c6931321f4793ae06fbb54
Jet Black <jetb250@gmail.com>
2019-06-18 18:18:19 -0500

    Added test case to module

--- animations.py
+++ animations.py
@@ -52,3 +52,36 @@ class Sprite(turtle.Turtle):
 def update():
     for sprite in sprites:
         sprite.update()
+
+if __name__ == "main":
+    import os
+
+    screen = turtle.Screen()
+    screen.tracer(0)
+
+    sprites = os.getcwd() + '/sprites'
+    os.chdir(sprites + '/left')
+    screen.addshape('duck_down_l.gif')
+    screen.addshape('duck_up_l.gif')
+    screen.addshape('idle_l.gif')
+    screen.addshape('jump_l.gif')
+    screen.addshape('move_right_l.gif')
+    screen.addshape('punch_l.gif')
+
+    states = {
+        'idle' : {
+            'delay' : 100,
+            'shapes' : ['duck_down_l.gif', 'duck_up_l.gif', 'idle_l.gif']
+        },
+        'attack' : {
+            'delay' : 100,
+            'shapes' : ['duck_down_l.gif', 'duck_up_l.gif', 'idle_l.gif']
+        },
+    }
+
+    shelly = Sprite(states)
+    shelly.set_state('idle')
+
+    while True:
+        update()
+        screen.update()
